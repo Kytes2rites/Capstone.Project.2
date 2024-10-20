1b1ccfb1af86f17ab848db27ff27a49943077bbc
kytes2rites <jilhoff61@outlook.com>
2024-10-20 16:40:56 -0500

    updated files

--- form.js
+++ form.js
@@ -1,11 +1,10 @@
 function myProcessForm(event) {
     event.preventDefault();
     const form = event.target;
-    const dateinput = form[0];
-    const phoneinput = form[1];
+    const phoneinput = form[0];
+    const dateinput = form[1];
     const nameinput = form[2];
-    const date = dateinput.value;
     const phone = phoneinput.value;
+    const date = dateinput.value;
     const name = nameinput.value;
-    output(` Thank you ${date}@${phone} . Your appointment is scheduled on ${name}!`);
-}
+    output(` ${name} @ ${phone} . ${name}!`);
