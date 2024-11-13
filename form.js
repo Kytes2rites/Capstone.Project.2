e5c4bcdcc3aa1e9d77916131017982a0de47756f
kytes2rites <jilhoff61@outlook.com>
2024-11-13 17:11:21 -0600

    added serverSimulation branch

--- form.js
+++ form.js
@@ -9,4 +9,37 @@ function myProcessForm(event) {
     const phone = dateinput.value;
     const date = nameinput.value;
     const time = timeinput.value;
-    output(` ${name}  @ ${phone} . ${time} at ${time}!`);
+    output(` ${name}  @ ${phone} . ${time} at ${time}!`)
+}
+
+    
+function handleSubmit(event) {
+    event.preventDefault();
+    const phoneForm = event.target;
+    const phoneInput = phoneForm[0];
+    const phoneValue = phoneInput.value;
+    output(` <br>Your info is being processed. . .<br>`);
+    const promise = new Promise(getServerResponse2);
+    promise.then(parseResponse);
+}
+    
+
+function getServerResponse2(resolve) {
+    setTimeout(activateResolve, 5000);
+    
+    function activateResolve() {
+        const response = {
+        message: "Successfully recieved. See you there!",
+        };
+        const resolveValue = JSON.stringify(response);
+        resolve(resolveValue);
+    }
+}
+    
+    
+
+function parseResponse(resolveValue) {
+    const response = JSON.parse(resolveValue);
+    const message = response.message;
+    output(message);
+}
