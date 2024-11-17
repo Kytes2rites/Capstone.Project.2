4264ab97e3f7819be62a62025ab7c625c80b9eed
kytes2rites <jilhoff61@outlook.com>
2024-11-17 05:27:49 -0600

    styling profiles

--- form.js
+++ form.js
@@ -11,34 +11,3 @@ function myProcessForm(event) {
     const time = timeinput.value;
     output(` ${name}  @ ${phone} . ${time} at ${time}!`)
 }
-
-    
-function handleSubmit(event) {
-    event.preventDefault();
-    const phoneForm = event.target;
-    const phoneInput = phoneForm[0];
-    const phoneValue = phoneInput.value;
-    output(` <br>Your info is being processed. . .<br>`);
-    const promise = new Promise(getServerResponse2);
-    promise.then(parseResponse);
-}
-    
-
-function getServerResponse2(resolve) {
-    setTimeout(activateResolve, 5000);
-    
-    function activateResolve() {
-        const response = {
-        message: "Recieved! We Will be in touch!",
-        };
-        const resolveValue = JSON.stringify(response);
-        resolve(resolveValue);
-    }
-}
-    
-    
-function parseResponse(resolveValue) {
-    const response = JSON.parse(resolveValue);
-    const message = response.message;
-    output(message);
-}
